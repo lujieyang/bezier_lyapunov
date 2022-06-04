b8618cb8856ebf381da3f00559c418104eac952a
lujieyang <lujie@mit.edu>
2022-06-04 18:10:22 -0400

    test acrobot sos swing up dynamics in eq(21)

--- acrobot/test_acrobot.py
+++ acrobot/test_acrobot.py
@@ -1,27 +1,31 @@
 import unittest
 import numpy as np
 from acrobot_swingup_fvi import acrobot_setup
+from acrobot_sos_swingup import acrobot_sos_lower_bound
 from pydrake.all import (Simulator, ContinuousState)
 from pydrake.examples.acrobot import (AcrobotPlant, AcrobotInput) 
 
 class TestAcrobot(unittest.TestCase):
     def test_Minv(self):
         Minv_val = params_dict["Minv"](x)
-        M = acrobot.MassMatrix(context)
-        np.testing.assert_allclose(Minv_val, np.linalg.inv(M))
+        np.testing.assert_allclose(Minv_val, np.linalg.inv(M_drake))
 
     def test_time_derivatives(self):
         Minv_val = params_dict["Minv"](x)
         T_val = params_dict["T"](params_dict["x2z"](x))
         # Remember to remove dependence on T
-        u = 1
         f_val = params_dict["f"](x, Minv_val, np.array([u]), T_val)
-        input = AcrobotInput()
-        input.set_tau(u)
-        acrobot.GetInputPort("elbow_torque").FixValue(context, input)
-        f = acrobot.AllocateTimeDerivatives()
-        acrobot.CalcTimeDerivatives(context, f)
         np.testing.assert_allclose(f_val.astype(float), T_val@f.CopyToVector())
+
+    def test_xdot(self):
+        M0, F, M = acrobot_sos_lower_bound(2, test=True)
+        x2z = params_dict["x2z"]
+        z = x2z(x)
+        np.testing.assert_allclose(M(z), M_drake)
+
+        M0_val = M0(z, float)
+        F_val = F(z, np.array([u]), float)
+        np.testing.assert_allclose(np.linalg.inv(M0_val)@F_val, f.CopyToVector())
 
 if __name__ == '__main__':
     acrobot = AcrobotPlant()
@@ -32,4 +36,11 @@ if __name__ == '__main__':
     x = np.random.random(4)
     state = context.get_mutable_continuous_state_vector().SetFromVector(x)
     params_dict = acrobot_setup()
+    u = 1
+    input = AcrobotInput()
+    input.set_tau(u)
+    acrobot.GetInputPort("elbow_torque").FixValue(context, input)
+    f = acrobot.AllocateTimeDerivatives()
+    acrobot.CalcTimeDerivatives(context, f)
+    M_drake = acrobot.MassMatrix(context)
     unittest.main()
